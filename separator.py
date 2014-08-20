c4da416640d6aee88b40c2e14f676f26ec5963ad
jollwish <jollwish@sina.cn>
2014-08-20 10:03:55 +0800

    Update separator.py

--- separator.py
+++ separator.py
@@ -4,10 +4,7 @@ from wavGet import signal
 from tool.FFT import *
 
 #Constants
-signal = signal[:10000]
-print len(signal)
 N0 = (len(signal)-KAPPA)//ETA
-print N0
 
 spectrogram = np.array([STFT(signal,i) for i in range(0,N0)]).transpose()
 U,s,V = npl.svd(spectrogram,full_matrices=False)
